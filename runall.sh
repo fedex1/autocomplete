4bba2d3501f2cb17b019b6d4a1f7246f47ee0a69
ralph yozzo <ralph@brooklynmarathon.com>
2024-06-15 16:32:31 -0400

    ralph-desktop

---
+++ runall.sh
@@ -0,0 +1,29 @@
+#!
+
+# issue:
+# 
+# npm ERR! code ERESOLVE
+# npm ERR! ERESOLVE could not resolve
+# npm ERR!
+# npm ERR! While resolving: eslint-plugin-jest@24.7.0
+# npm ERR! Found: @typescript-eslint/eslint-plugin@2.34.0
+# npm ERR! node_modules/@typescript-eslint/eslint-plugin
+# npm ERR!   dev @typescript-eslint/eslint-plugin@"2.34.0" from the root project
+# npm ERR!
+# npm ERR! Could not resolve dependency:
+# npm ERR! peerOptional @typescript-eslint/eslint-plugin@">= 4" from eslint-plugin-jest@24.7.0
+# npm ERR! node_modules/eslint-plugin-jest
+# npm ERR!   dev eslint-plugin-jest@"24.7.0" from the root project
+# npm ERR!   eslint-plugin-jest@"^24.1.0" from react-scripts@4.0.3
+# npm ERR!   node_modules/react-scripts
+# npm ERR!     dev react-scripts@"4.0.3" from @algolia/autocomplete-example-react-18@1.17.2
+# npm ERR!     examples/react-18
+# npm ERR!       @algolia/autocomplete-example-react-18@1.17.2
+# npm ERR!       node_modules/@algolia/autocomplete-example-react-18
+# npm ERR!     3 more (@algolia/autocomplete-example-react-renderer, ...)
+# npm ERR!
+# 
+# curl --compressed -o- -L https://yarnpkg.com/install.sh | bash
+yarn
+yarn workspace @algolia/autocomplete-example-instantsearch start
+
